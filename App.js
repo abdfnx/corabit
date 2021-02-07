d6fe188a04a4836e1d96ba82f36f3b11f8bc28ac
abdfnx <64256993+abdfnx@users.noreply.github.com>
2021-02-07 15:01:44 +0300

    restore

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import AppCLI from "./app/src/components/App";
 
 export default function App() {
-  return (
-    <AppCLI />
-  );
+  // return (
+  //   <AppCLI />
+  // );
 }
